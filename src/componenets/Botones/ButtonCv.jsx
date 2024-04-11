ba7640f3fd37c618a252cf95d02b4882e6c8f5b9
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-04-11 11:22:04 -0300

    cv

--- src/componenets/Botones/ButtonCv.jsx
+++ src/componenets/Botones/ButtonCv.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactTooltip from "react-tooltip";
-import CV from "../../assets/CV/CVMaxi.pdf";
+import CV from "../../assets/CV/CV-Maxi-C.pdf";
 import dow from "../../IMG/download_FILL0_wght400_GRAD0_opsz48.png";
 import "./Button.css";
 
@@ -8,7 +8,7 @@ export const ButtonCv = () => {
     return (
         <div className="buttons">
             <button className="boton" data-tip data-for="botonsobre">
-                <a href={CV} download="CV-Maxi">
+                <a href={CV} download="CV-MaxiC">
                     <span className="shadow"></span>
                     <span className="edge"></span>
                     <span className="front text">
